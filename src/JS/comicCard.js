a920005c60d18d25c6fa58b149bd028fd1b6492b
Diego Jarauta <diegojarauta8@gmail.com>
2021-01-24 20:47:36 +0100

    terminado visualizacion comics para ordenador

--- src/JS/comicCard.js
+++ src/JS/comicCard.js
@@ -24,65 +24,80 @@ function ComicCard() {
     })
 },[id])
 
+/* ----------- */
+/* BOTON CESTA */
+/* ----------- */
+const cartButtons = document.querySelectorAll('.cart-button');
+
+cartButtons.forEach(button => {
+	button.addEventListener('click', cartClick);
+});
+
+function cartClick() {
+	let button = this;
+	button.classList.add('clicked');
+}
+/* --------------- */
+/* FIN BOTON CESTA */
+/* --------------- */
+
+function favorito (titulo) {
+    console.log(titulo)
+    return(<BrowserRouter>
+    
+    
+    </BrowserRouter>
+  )}
+
 let mostrarImagen = imagen.map(mostrar=>{
     if (contador <=0) {
         contador = contador + 1
-        return(<><div className="left-image" style={{backgroundImage:`url("${mostrar.path}.${mostrar.extension}")`}} alt="" height="400"></div></>)
+        return(<><div className="left-image" style={{backgroundImage:`url("${mostrar.path}.${mostrar.extension}")`}} alt="" height="400"><Link to="/comics"><button className="close-button">regresar</button></Link></div></>)
     }
     })
 
 
   return(<div className="comicCard">
             <div className="card-container">
-                
+            
                     {mostrarImagen}
                
                 <div className="container-right">
                     <div className="top-section">
                         <div className="title-content">
                             <h1>{data.title}</h1>
-                            <p><span className="rating">R</span>| <span className="studio">Marvel Studios</span>|<img src="https://staticv2.rottentomatoes.com/static/images/icons/fresh-16.png" height="16px" width="16px" />
-                            <span className="score">95%</span></p>
+                            <p className="detalles-comic"><span className="rating">{data.variantDescription}</span>| <span className="studio">Marvel Studios</span>|<span>Favorito 
+                           {/*  <div className="Category-comic"> */}
+                  <label className="like">
+                   <input onClick={()=>{favorito(data.title)}} type="checkbox"/>
+                    <span className="material-icons heart">favorite</span>
+                    {/* https://google.github.io/material-design-icons/ */}
+                    {/* https://material.io/resources/icons/?style=baseline */}
+                     <div className="ripple"></div>
+                  </label>
+                {/* </div> */}
+                            </span>
+                            </p>
                         </div>
                     </div>
                     <div className="description">
                         <p>{data.description}</p>
                     </div>
-                    <div className="favorite">
-                        <div className="star-icon">
-                            <svg width="14px" height="13px" viewBox="0 0 14 13" version="1.1">
-                                <g>
-                                    <polygon id="Star" points="7 10.5 2.88550323 12.663119 3.67130219 8.08155948 0.342604386 4.83688104 4.94275162 4.16844052 7 0 9.05724838 4.16844052 13.6573956 4.83688104 10.3286978 8.08155948 11.1144968 12.663119"></polygon>
-                                </g>
-                            </svg>
-                        </div>
-                        <p>Favorite</p>
-                    </div>
-                    <div className="wishlist">
-                        <div className="list-icon">
-                            <svg width="9px" height="8px" viewBox="0 0 9 8" version="1.1">
-                                <g>
-                                    <path d="M0,0 L9,0 L9,2 L0,2 L0,0 Z M0,3 L9,3 L9,5 L0,5 L0,3 Z M0,6 L9,6 L9,8 L0,8 L0,6 Z" id="Combined-Shape" ></path>
-                                </g>
-                            </svg>
-                        </div>
-                        <p>Add to Wish List</p>
-                    </div>
-                    <div className="button outline">
-                        <p className="price">HD - $4.99</p>
-                        <div className="arrow">
-                            <svg width="8px" height="4px" viewBox="0 0 8 4" version="1.1">
-                                <g >
-                                    <polyline id="arrow" stroke="#145ce0" points="1 0.118013258 3.96325692 3.08127018 7.04452709 0"></polyline>
-                                </g>
-                            </svg>
-                        </div>
-                    </div>
-                    <div className="button full">
-                        <p>RENT</p>
+                    
+                    
+                    
+            </div>
+                    <div
+                     className="boton-cesta full">
+
+<button class="cart-button">
+	<span class="add-to-cart">Añadir a la cesta</span>
+	<span class="added">Añadido</span>
+	<i class="fas fa-shopping-cart"></i>
+	<i class="fas fa-box"></i>
+</button>
                     </div>
                 </div>
-            </div>
         </div>)
  
 }
--- src/JS/comics.js
+++ src/JS/comics.js
@@ -7,15 +7,19 @@ function Comics () {
 
     let [data,setData] = useState([])
     let [info,setInfo] = useState([])
+    let [isLoading, setIsLoading] = useState(false)
+    let contador = 0
     
   
     useEffect(function(){
       
+        setIsLoading(true)
         fetch(`http://gateway.marvel.com/v1/public/comics?ts=1&apikey=70945025a9ff9a1f3d2c54de4c8c3599&hash=a92c5e400dfca9dbc06a81b098cf6210`).then(respuesta=>respuesta.json()).then(datos=>{
-
-       setData(datos)
-       setInfo(datos.data.results)
-       console.log(datos.data.results[3])
+            
+            setData(datos)
+            setInfo(datos.data.results)
+            console.log(datos.data.results[3])
+            setIsLoading(false)
         
       })
     },[])
@@ -31,26 +35,65 @@ function Comics () {
     let element = document.getElementById("animate");
 
     let mostrarComics= info.map(resultados=>{
+                                    {contador = 0}
         return(<>
-        <div className="panel">
-        <Link to={`/comics/${resultados.id}`}>
-            <div className="flip">
-  <div className="front">
-  {resultados.images.map(mostrar=>{return(<><img src={`${mostrar.path}.${mostrar.extension}`} alt="" height="400"/></>)})}
-{/*  <img src={`${resultados.images[0].path}.${resultados.images[0].extension}`} alt="" height="400"/> */}
-    <p className="text top-left">{resultados.title}</p>
-    <p className="text bottom-right">{resultados.prices[0].price} €</p>
-  </div>
- <div className="back">
-      <p className="text speech">{resultados.description}</p>
-  </div> 
-  </div>
-  </Link>
-  </div>
+                <div className="panel">
+                    <Link to={`/comics/${resultados.id}`}>
+                                    {resultados.images.map(mostrar=>{if (contador <= 0) {contador = contador + 1
+                                            return(<><img className="imagen-comic" src={`${mostrar.path}.${mostrar.extension}`} alt="" width="300" />
+                                            </>)}
+                                    })}
+                                    <p className="text top-left">{resultados.title}</p>
+                                    <p className="text bottom-right">{resultados.prices[0].price} €</p>
+                               
+                    </Link>
+                </div>
         </>)
     })
 
     
+
+    if (isLoading) {
+        return(<>
+        <header className="intro">
+        <div className="wrapper run-animation" id="animate">
+	        <div className="logo">
+                <span className="marvel">Marvel</span>
+		        <span className="studios">Studios</span>
+            </div>
+        </div>
+        <div className="images"></div>
+    </header>
+
+    <div className="container">
+        <div className="row">
+            <div className="col-1">
+                
+                    <div className="box box1">
+                        <div className="oddboxinner"> bangers?</div>
+                    </div>
+                
+            </div>
+            <div className="col-10">
+              
+
+                
+            </div>
+            <div className="col-1">
+                
+                <div className="box box1">
+                    <div className="oddboxinner"> bangers?</div>
+                </div>
+               
+            </div>
+        </div>
+    </div>
+    
+        
+    <footer><p>{data.attributionText}</p></footer>
+        </>)
+
+    } else {
     return(<>
     <header className="intro">
         <div className="wrapper run-animation" id="animate">
@@ -65,19 +108,25 @@ function Comics () {
     <div className="container">
         <div className="row">
             <div className="col-1">
-                <div className="box box1">
-                    
-                </div>
+                
+                    <div className="box box1">
+                        <div className="oddboxinner"> bangers?</div>
+                    </div>
+                
             </div>
             <div className="col-10">
+              
                 <article className="comic">
                     {mostrarComics}
                 </article>
+                
             </div>
             <div className="col-1">
-            <div className="box box1">
+                
+                <div className="box box1">
                     <div className="oddboxinner"> bangers?</div>
                 </div>
+               
             </div>
         </div>
     </div>
@@ -85,6 +134,7 @@ function Comics () {
         
     <footer><p>{data.attributionText}</p></footer>
     </>)
+    }
 }
 
 export default Comics;
