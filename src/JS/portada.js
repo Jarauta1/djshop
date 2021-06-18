049edf8241dc7d9154639fc350b4440e294c9543
jarauta1 <diegojarauta8@gmail.com>
2021-06-18 10:46:11 +0200

    Cambio tamaño imagenes portada para diseño responsive Listo para implementar en produccion

--- src/JS/portada.js
+++ src/JS/portada.js
@@ -13,32 +13,42 @@ import {Link} from "react-router-dom"
 function Portada () {
   localStorage.setItem("retorno", "")
     return(<>
-    <div className="container-portada">
-      <div className="row-portada">
-      <div className="div-portada starred">
-      <Link to="/camisetas"><h1 className="starred-title">CAMISETAS</h1>
-            <img className="img-portada" src={camisetas} alt=""/></Link>
+      <div className="container-portada">
+        <div className="row-portada">
+          <div className="div-portada starred">
+            <Link to="/camisetas">
+              <h1 className="starred-title">CAMISETAS</h1>
+              <img className="img-portada" src={camisetas} alt=""/>
+            </Link>
+          </div>
+          <div className="div-portada starred">
+            <Link to="/comics">
+              <h1 className="starred-title">COMICS</h1>
+              <img className="img-portada" src={comics} alt=""/>
+            </Link>
+          </div>
+          <div className="div-portada starred">
+            <Link to="/libros">
+              <h1 className="starred-title">LIBROS</h1>
+              <img className="img-portada" src={libros} alt=""/>
+            </Link>
+          </div>
         </div>
-        <div className="div-portada starred">
-      <Link to="/comics"><h1 className="starred-title">COMICS</h1>
-            <img className="img-portada" src={comics} alt=""/></Link>
-        </div>
-        <div className="div-portada starred">
-      <Link to="/libros"><h1 className="starred-title">LIBROS</h1>
-            <img className="img-portada" src={libros} alt=""/></Link>
+        <div className="row-portada">
+          <div className="div-portada-50 starred">
+            <Link to="/cine">
+              <h1 className="starred-title">CINE</h1>
+              <img className="img-portada" src={cine} alt=""/>
+            </Link>
+          </div>
+          <div className="div-portada-50 starred">
+            <Link to="/zapatillas">
+              <h1 className="starred-title">ZAPATILLAS</h1>
+              <img className="img-portada" src={zapatillas} alt=""/>
+            </Link>
+          </div>
         </div>
       </div>
-      <div className="row-portada">
-      <div className="div-portada-50 starred">
-      <Link to="/cine"><h1 className="starred-title">CINE</h1>
-            <img className="img-portada" src={cine} alt=""/></Link>
-        </div>
-        <div className="div-portada-50 starred">
-      <Link to="/zapatillas"><h1 className="starred-title">ZAPATILLAS</h1>
-            <img className="img-portada" src={zapatillas} alt=""/></Link>
-        </div>
-      </div>
-    </div>
 		</>);
 }
 
